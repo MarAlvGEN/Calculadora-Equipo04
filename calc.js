a0ced65cbb91b9156b24db1095ff2055d89a9bf1
Mauricio Alvarez <297007275+MarAlvGEN@users.noreply.github.com>
2026-07-01 12:29:49 -0500

    MOD: Taller

--- calc.js
+++ calc.js
@@ -1,49 +1,51 @@
-import promptSync from 'prompt-sync';
+import promptSync from "prompt-sync";
 const prompt = promptSync();
-
-
-
-
 
 let resultado = null;
 let Salir = false;
 
+let numValid;
 
 while (Salir == false) {
 
-    console.log("CALCULADORA: Elige números y operadores! (Escribe exit para salir)");
-    let num1 = prompt("Número 1: ");
+  numValid = true
+  console.log(
+    "CALCULADORA: Elige números y operadores! (Escribe exit para salir)",
+  );
+  let num1 = prompt("Número 1: ");
 
-    if (num1 == "exit") {
-        Salir = true
-    } else {
+  if (num1 == "exit") {
+    Salir = true;
+  } else {
+    let oper = prompt("Selecciona un operador (+ - * /): ");
 
-        let oper = prompt("Selecciona un operador (+ - * /): ");
-
-        num1 = Number(num1);
-        let num2 = prompt("Número 2: ");
-        num2 = Number(num2);
+    num1 = Number(num1);
+    let num2 = prompt("Número 2: ");
+    num2 = Number(num2);
 
     if (oper === "+") {
-        resultado = num1 + num2;
+      resultado = num1 + num2;
     } else if (oper === "-") {
-        resultado = num1 - num2;
+      resultado = num1 - num2;
     } else if (oper === "*") {
-        resultado = num1 * num2;
+      resultado = num1 * num2;
     } else if (oper === "/") {
-        if (num2 === 0) {
-            console.log("No se puede dividir por cero.");
-        }
-        resultado = num1 / num2;
+      if (num2 === 0) {
+        console.log("No se puede dividir por cero.");
+        numValid = false
+            }
+      resultado = num1 / num2;
     } else {
-        console.log("OPERADOR NO VÁLIDO");
+      console.log("OPERADOR NO VÁLIDO");
     }
-    }
+  }
 
-    if (resultado != null) {
-        console.log("El resultado es: ", resultado)
-    }
+  if (resultado != null && numValid == true) {
+    console.log("El resultado es: ", resultado);
+    console.log("----------------------------");
+  }
+
+
 }
 
-console.log("Saliendo del programa...")
-
+console.log("Saliendo del programa...");
